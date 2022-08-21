e3beaa252d88c892a1aabb942dab06990b2e49d2
Mahdi03 <damahdi03@gmail.com>
2022-08-20 23:13:13 -0700

    Worked on adding storage logic to dietary preferences page

--- scripts/background.js
+++ scripts/background.js
@@ -8,7 +8,7 @@ fetch(chrome.runtime.getURL('diets.json')) // get all diets.json data
 
 //Open the configuration page on install
 chrome.runtime.onInstalled.addListener(async() => {
-    let url = chrome.runtime.getURL("diet_restriction.html"); //Gets the full URL
+    let url = chrome.runtime.getURL("configMenu.html"); //Gets the full URL
     let tab = await chrome.tabs.create({ url }); //Opens the file in a new tab
 });
 
